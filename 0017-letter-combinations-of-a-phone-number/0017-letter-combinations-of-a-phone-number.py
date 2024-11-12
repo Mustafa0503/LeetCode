c9e8be22ea52b9ea45cde9e92103088c441a7695
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2024-11-11 22:45:40 -0500

    Time: 0 ms (100%), Space: 16.7 MB (33.31%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,9 +1,10 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
+        if(digits == ""):
             return []
-
-        phone_map = {
+        ls = []
+        let = []
+        d = {
             '2': 'abc',
             '3': 'def',
             '4': 'ghi',
@@ -13,13 +14,20 @@ class Solution:
             '8': 'tuv',
             '9': 'wxyz'
         }
-        combinations = [""]
+        if(len(digits)==1):
+            return list(d[digits])
+        s=""
+        for i in digits:
+            for j in d[i]:
+                s=s+j
+            
+            let.append(s)
+            s=""
+        s=""
+        combinations = [''.join(chars) for chars in product(*let)]
+        print(combinations)
+                   
+                
 
-        for digit in digits:
-            new_combinations = []
-            for combination in combinations:
-                for letter in phone_map[digit]:
-                    new_combinations.append(combination + letter)
-            combinations = new_combinations
-
+                
         return combinations
