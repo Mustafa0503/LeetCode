2a0093a09c952320e2956b8c4b12ed716c2cbc73
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2024-11-12 23:01:18 -0500

    Time: 15 ms (9.47%), Space: 16.9 MB (12.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,12 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        if(len(s)==0):
+            return True
+        if(len(s)%2!=0):
+            return False
         stack = []
-        bracket_map = {')': '(', '}': '{', ']': '['}
-        for char in s:
-            if char in bracket_map:
-                top_element = stack.pop() if stack else '#'
-                if bracket_map[char] != top_element:
-                    return False
+        hit = len(s)/2
+        count=0
+      
+        mapping = {')': '(', '}': '{', ']': '['}
+        for i in s:
+            if(i not in mapping):
+                stack.insert(0,i)
             else:
-                stack.append(char)
-        return not stack
+                if(len(stack)!=0):
+                    if(stack[0]!=mapping[i]):
+                        return False
+                    stack = stack[1:]
+                    count+=1
+        if(len(stack)==0 and hit==count):
+            return True
+        return False
