d8a10cfe17eeffd477123d68531f6871ccdef8cc
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2024-11-12 22:50:20 -0500

    Time: 19 ms (9.47%), Space: 17 MB (12.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,29 +3,25 @@ class Solution:
         stack = []
         hit = len(s)/2
         count=0
-
+        op=["(","[","{"]
         if(len(s)==0):
             return True
         if(len(s)%2!=0):
             return False
         for i in s:
            
-            if(i=="(" or i == "{" or i== "["):
+            if(i in op):
                 
                 stack.insert(0,i)
             else:
                 
                 if(len(stack)!=0):
-                    
-                    
                     if(stack[0]=="(" and i!=")" or stack[0]=="{" and i!="}" or stack[0]=="[" and i!="]"):
                         return False
                     print(count)
                     stack = stack[1:]
                     count+=1
-        
 
-            
         if(len(stack)==0 and hit==count):
             return True
         return False
