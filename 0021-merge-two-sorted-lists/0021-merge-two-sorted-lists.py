a74ba29eeade105122732b8badecf8f8f267f910
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2024-11-14 15:58:46 -0500

    Time: 1 ms (21.25%), Space: 16.7 MB (32.35%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -9,20 +9,12 @@ class Solution:
         l2=list2
         head = ListNode(0)
         l3 = head
-        if(l1==None):
-            return l2
-        if(l2==None):
-            return l1
         while(l1!=None and l2!=None):
-            print(l1.val,l2.val)
-            print(l1.val==l2.val)
             if(l1.val<l2.val):
-                print("no")
                 l3.next = ListNode(l1.val)
                 l3=l3.next
                 l1=l1.next
             elif(l1.val==l2.val):
-                print("what")
                 l3.next = ListNode(l1.val)
                 l3=l3.next
                 l3.next = ListNode(l2.val)
@@ -30,16 +22,13 @@ class Solution:
                 l1=l1.next
                 l2=l2.next
             elif(l1.val>l2.val):
-                print("yop")
                 l3.next = ListNode(l2.val)
                 l3=l3.next
                 l2=l2.next
         
         if(l1!=None):
-            print("yp")
             l3.next= l1
             l3=l3.next
         if(l2!=None):
-            print("tt")
             l3.next= l2
         return head.next
