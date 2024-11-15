45581b6437e4a5d91825b50247ef5892165cdafa
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2024-11-14 22:18:00 -0500

    Time: 6183 ms (5%), Space: 20.3 MB (5.65%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -13,7 +13,6 @@ class Solution:
             return None
         for i in range(len(lists)-1):
             
-            l3 = head
             l2=lists[i+1]
             while(l1!=None and l2!=None):
                 if(l1.val<=l2.val):
@@ -28,7 +27,11 @@ class Solution:
                 l3.next= l1
             if(l2!=None):
                 l3.next= l2
-           
+            l3 = head
             l1=head.next
-            yo = l3
+            yo = l1
+            # while(yo):
+            #     print(yo.val)
+            #     yo = yo.next
+            # print("********************************")
         return l1
