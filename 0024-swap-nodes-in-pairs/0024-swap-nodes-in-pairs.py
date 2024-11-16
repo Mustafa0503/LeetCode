3798d43e44ecac35dd2eb4f2647a6af668f4d213
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2024-11-15 22:06:53 -0500

    Time: 3 ms (5.29%), Space: 16.6 MB (57.22%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -23,7 +23,7 @@ class Solution:
         l1 = head
         l2 = head.next
         prev = None
-        while(l2):
+        while(l1.next):
             if(prev): prev.next = l2
             temp = l2.next
             l2.next = l1
