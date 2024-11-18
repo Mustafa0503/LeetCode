293abeee92df271733d01d65f20878de457cab9f
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2024-11-18 18:43:22 -0500

    Time: 5 ms (8.84%), Space: 17.9 MB (5.54%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -24,7 +24,6 @@ class Solution:
             group = k
             arr.append(one)
             one = []
-        print(arr)
         # fin = new_head.next
         for i in arr:
             new_head = last_head
@@ -39,7 +38,6 @@ class Solution:
                 fin.next = last_head
                 fin=fin.next
                 last_head = last_head.next
-        print(le)
         if(k!=1 and le%k!=0):
             for i in arr[-1]:
                 fin.next = ListNode(i)
