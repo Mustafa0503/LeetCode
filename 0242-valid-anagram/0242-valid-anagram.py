411d695066d46fb5e4420d6bed5865c2c9ba821b
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2024-11-19 14:04:49 -0500

    Time: 15 ms (49.02%), Space: 16.8 MB (64.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
-        if len(s) != len(t):
+        if(len(s)!=len(t)):
             return False
+        sd={}
+        td={}
+        for i in range(len(t)):
+            sd[s[i]]=1+sd.get(s[i],0)  #.get return corresp val if dont exist then 0
+            td[t[i]]=1+td.get(t[i],0)
 
-        count = [0] * 26
-        for i in range(len(s)):
-            count[ord(s[i]) - ord('a')] += 1
-            count[ord(t[i]) - ord('a')] -= 1
-
-        for val in count:
-            if val != 0:
-                return False
-        return True
-
+        return sd==td
             
 
         
