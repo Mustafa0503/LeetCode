9dface920fd30278cb72e689c39e23eefa69714c
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2025-01-18 13:49:15 -0500

    Time: 147 ms (75.71%), Space: 37.8 MB (60.88%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,29 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pairs = []
-        stack = []
-        
-        # Create pairs of position and speed for each car
-        for x in range(len(speed)):
-            pairs.append([position[x], speed[x]])
-        
-        # Sort the pairs based on position in descending order
-        pairs = sorted(pairs)[::-1]
-        
-        # Iterate through the sorted pairs
-        for p, s in pairs:
-            # Calculate time to reach the target for the current car
-            time = (target - p) / s
-            
-            # Add the time to the stack
-            stack.append(time)
-            
-            # Check if there are at least two cars in the stack and the current car
-            # takes less or equal time than the previous car. If so, pop the previous car.
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
-                stack.pop()
-        
-        # The length of the stack represents the number of car fleets
+        stack=[]
+        tup=[]
+        tup=list((zip(position,speed)))
+        tup.sort(reverse=True)
+        for i in tup:
+            if(not stack):
+                stack.append(i)
+            else:
+                time_car = (target - i[0])/i[1]
+                time_car_st = (target-stack[-1][0])/stack[-1][1]
+                if(time_car>time_car_st):
+                    stack.append(i)
         return len(stack)
-
-        
