09ad9f7695cbb7d98ef3f6103f5f9e91fe7e575f
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2025-01-21 23:39:15 -0500

    Time: 240 ms (5%), Space: 75.8 MB (10.94%) - LeetHub

---
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -0,0 +1,48 @@
+class TimeMap:
+
+    def __init__(self):
+        self.dic = {}
+    def set(self, key: str, value: str, timestamp: int) -> None:
+            if(key in self.dic):
+                self.dic[key].append([value,timestamp])
+            else:
+                self.dic[key] = [[value,timestamp]]
+
+    def get(self, key: str, timestamp: int) -> str:
+        if(not key in self.dic):
+            return ""
+        l,r = 0,len(self.dic[key])-1
+        if(self.dic[key][0][1]>timestamp):
+            return ""
+
+        while(l<=r):
+            mid = (r-l)//2+l
+            if(self.dic[key][mid][1]==timestamp):
+                return self.dic[key][mid][0]
+            if(self.dic[key][mid][1]<timestamp):
+                l = mid+1
+            if(self.dic[key][mid][1]>timestamp):
+                r = mid -1
+        return self.dic[key][l-1][0]
+        # while(not timestamp in self.sett and timestamp>0):
+        #     print(timestamp)
+        #     if(timestamp<self.arr[0][-1]):
+        #         break
+        #     timestamp -=1
+        
+        # l,r = 0, len(self.arr)-1
+        # while(l<=r):
+        #     mid = (r-l)//2+l
+        #     if(self.arr[mid][-1]==timestamp):
+        #         return self.arr[mid][1]
+        #     if(self.arr[mid][-1]<timestamp):
+        #         l=mid+1
+        #     if(self.arr[mid][-1]>timestamp):
+        #         r=mid-1
+        return ""
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
