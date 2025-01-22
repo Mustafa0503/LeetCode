4ed46a7169365f3c9b416159b407b0135646ee82
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2025-01-22 00:01:30 -0500

    Time: 140 ms (37.79%), Space: 74.7 MB (57.18%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -3,27 +3,26 @@ class TimeMap:
     def __init__(self):
         self.dic = {}
     def set(self, key: str, value: str, timestamp: int) -> None:
-            if(key in self.dic):
-                self.dic[key].append([value,timestamp])
-            else:
-                self.dic[key] = [[value,timestamp]]
-
+        if key not in self.dic:
+            self.dic[key] = []
+        self.dic[key].append((value, timestamp))  # Store as a tuple for clarity
     def get(self, key: str, timestamp: int) -> str:
         if(not key in self.dic):
             return ""
         l,r = 0,len(self.dic[key])-1
         if(self.dic[key][0][1]>timestamp):
             return ""
-
+        val = self.dic[key]
         while(l<=r):
             mid = (r-l)//2+l
-            if(self.dic[key][mid][1]==timestamp):
+            if(val[mid][1]==timestamp):
                 return self.dic[key][mid][0]
-            if(self.dic[key][mid][1]<timestamp):
+            if(val[mid][1]<timestamp):
                 l = mid+1
-            if(self.dic[key][mid][1]>timestamp):
+
+            else:
                 r = mid -1
-        return self.dic[key][l-1][0]
+        return self.dic[key][r][0]
         # while(not timestamp in self.sett and timestamp>0):
         #     print(timestamp)
         #     if(timestamp<self.arr[0][-1]):
