15bbe039c1573c4aa8119cb1d99a3c8b24e8e569
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2025-11-11 12:52:00 -0500

    Time: 223 ms (5.16%), Space: 34.6 MB (20.23%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,22 +1,20 @@
 class Solution:
+
     def partition(self, s: str) -> List[List[str]]:
-        n = len(s)
-        isPal = [[False] * n for _ in range(n)]
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                if s[i] == s[j] and (j - i < 2 or isPal[i + 1][j - 1]):
-                    isPal[i][j] = True
-    
-        res = []
-        path = []
-        def dfs(start):
-            if start == n:
-                res.append(path.copy())
+        def palindrome(s):
+            return s == s[::-1] 
+        ans = []
+        temp=[]
+        def dfs(ind):
+            print("temp: ",temp,"ind: ",ind)
+            if ind == len(s):
+                ans.append(temp.copy())
                 return
-            for end in range(start, n):
-                if isPal[start][end]:
-                    path.append(s[start:end + 1])
-                    dfs(end + 1)
-                    path.pop()
+            print("got here")
+            for i in range(ind,len(s)):
+                if(palindrome(s[ind:i+1])):
+                    temp.append(s[ind:i+1])
+                    dfs(i+1)
+                    temp.pop()
         dfs(0)
-        return res
+        return ans
