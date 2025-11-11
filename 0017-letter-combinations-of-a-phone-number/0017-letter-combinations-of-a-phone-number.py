94f2a7bc3061d64b78244cf55f5504e745e0338b
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2025-11-11 13:53:04 -0500

    Time: 0 ms (100%), Space: 18.1 MB (22.51%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,37 +1,24 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-
-        ls = []
-        let = []
-        d = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz'
+        phone_board = {
+            1: [],                2: ['a', 'b','c'],    3: ['d','e','f'],
+            4: ['g','h','i'],     5: ['j','k','l'],     6: ['m','n','o'],
+            7: ['p','q','r','s'], 8: ['t','u','v'],     9: ['w','x','y','z']
         }
-        if(len(digits)==0):
-            return []
-        s=""
-        combinations=[""]
-        
+
+        ans = []
+        temp = []
+        letters = []
         for i in digits:
-            new_comb=[]
-          
-
-            for comb in combinations:
-                print("comb: ",comb)
-
-                for letter in d[i]:
-                   
-                    new_comb.append(comb + letter)
-                
-            combinations = new_comb                   
-                
-
-
-                
-        return combinations
+            letters.append(phone_board.get(int(i)))
+        print(letters)
+        def dfs(ind):
+            if len(temp)==len(digits):
+                ans.append("".join(temp.copy()))
+                return
+            for i in letters[ind]:
+                temp.append(i)
+                dfs(ind+1)
+                temp.pop()
+        dfs(0)
+        return ans
